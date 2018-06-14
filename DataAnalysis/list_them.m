96b8c4e6409c7945aa6e5e01507e315a8fe7ca9b
Ben Roque <broque@clarku.edu>
2018-06-14 13:55:46 -0400

    ?

--- DataAnalysis/list_them.m
+++ DataAnalysis/list_them.m
@@ -1,10 +1,10 @@
 function num=list_them(name)
 a=ls(name)
-a(2,:)
+% a(2,:)
 
 k=0;
 for i=1:length(a)
-   IND=strfind(a(i,:),'bmp');
+   IND=strfind(a(i),'bmp');
    if ~isempty(IND)
        k=k+1;
        num(k)=str2double(a(i,(IND-6):(IND-2)));
